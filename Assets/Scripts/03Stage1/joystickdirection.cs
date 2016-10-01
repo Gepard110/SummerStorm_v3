fb3fb195c204bac6198388b5231d61823e3eed0e
BuildTools <hiro-spica830@i.softbank.jp>
2016-10-01 20:41:41 +0900

    10/1

--- Assets/Scripts/03Stage1/joystickdirection.cs
+++ Assets/Scripts/03Stage1/joystickdirection.cs
@@ -11,50 +11,8 @@ public class joystickdirection : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		direction = CrossPlatformInputManager.GetAxis ("V") / CrossPlatformInputManager.GetAxis ("H");
-
-	if (CrossPlatformInputManager.GetAxis ("H") >= 0) {
-			if (direction > 1) {
-				transform.Rotate (new Vector3 (0f, 0f, 0f));
-			}
-
-			if (direction > 0.5 && direction < 1) {
-				transform.Rotate (new Vector3 (0f, 45f, 0f));
-			}
-
-			if (direction > -0.5 && direction < 0.5) {
-				transform.Rotate (new Vector3 (0f, 90f, 0f));
-			}
-
-			if (direction > -1 && direction < -0.5) {
-				transform.Rotate (new Vector3 (0f, 135f, 0f));
-			}
-
-			if (direction < -1) {
-				transform.Rotate (new Vector3 (0f, 180f, 0f));
-			}
-		}
-
-		if (CrossPlatformInputManager.GetAxis ("H") <= 0) {
-			if (direction > 1) {
-				transform.Rotate (new Vector3 (0f, 180f, 0f));
-			}
-
-			if (direction > 0.5 && direction < 1) {
-				transform.Rotate (new Vector3 (0f, 225f, 0f));
-			}
-
-			if (direction > -0.5 && direction < 0.5) {
-				transform.Rotate (new Vector3 (0f, 270f, 0f));
-			}
-
-			if (direction > -1 && direction < -0.5) {
-				transform.Rotate (new Vector3 (0f, 315f, 0f));
-			}
-
-			if (direction < -1) {
-				transform.Rotate (new Vector3 (0f, 135f, 0f));
-			}
-		}
+		float angle = Mathf.Atan2(CrossPlatformInputManager.GetAxis("V"),CrossPlatformInputManager.GetAxis("H"));
+		direction = Mathf.RoundToInt (angle / Mathf.PI*180);
+		this.gameObject.transform.eulerAngles = new Vector3(0,0,direction - 90);
 	}
 }
